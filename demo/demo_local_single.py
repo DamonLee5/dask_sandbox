7b8d27ba9028422cbf723a8fbd04ac2703bf94c2
Diyu Yang <yang1467@purdue.edu>
2021-07-23 13:23:57 -0400

    explanations on workers and threads

--- demo/demo_local_single.py
+++ demo/demo_local_single.py
@@ -9,18 +9,22 @@ from dask.distributed import Client, as_completed, LocalCluster
 
 # Define function that returns dictionary containing host name, PID, and time.
 def slow_increment(x):
-    time.sleep(1)
-    return {'result': x + 1, # function output
+    time.sleep(2)
+    return {
+            'input': x, # function input
+            'result': x + 1, # function output
             'host': socket.gethostname(), # host name
             'pid': os.getpid(), # process ID
             'time': time.strftime("%H:%M:%S")} # time stamp
 
 if __name__ == '__main__':
     # Set processes=True - Multiple independence processes with different process IDs will be used.
-    # Set n_workers=2 - Number of workers to start
-    
     # Reference: `https://docs.dask.org/en/latest/setup/single-distributed.html?highlight=LocalCluster#localcluster`
-    cluster = LocalCluster(processes=True ,n_workers=4) # This creates a “cluster” of a scheduler and workers running on the local machine.
+    # Set n_workers=4 - Use 4 processors. 
+    # Set threads_per_worker=2: Number of threads per worker
+    # Total number of jobs computed parallelly is n_workers*threads_per_worker
+    # In this case you will see 8 jobs run parallelly at the same time.
+    cluster = LocalCluster(processes=True ,n_workers=4, threads_per_worker=2) # This creates a “cluster” of a scheduler and workers running on the local machine.
     
     # reference: `https://distributed.dask.org/en/latest/api.html#distributed.Client`
     client = Client(cluster) # An interface that connect to and submit computation to cluster
@@ -28,13 +32,13 @@ if __name__ == '__main__':
 
     # Call the function slow_increments with 16 different arguments and map to workers
      
+    # client.map() map the function "slow_increment" to a sequence of input arguments.
+    # reference: `https://distributed.dask.org/en/latest/api.html#distributed.Client.map`
     # The class object as_completed() returns outputs in the order in which they complete
     # reference: `https://distributed.dask.org/en/latest/api.html#distributed.as_completed`
-    
-    # client.map map the function "slow_increment" to a sequence of input arguments.
-    # reference: `https://distributed.dask.org/en/latest/api.html#distributed.Client.map`
     for output in as_completed(client.map(slow_increment, range(16))): 
         # Collect output of each job and combin into result
         result = output.result()
         # Print the result and time stamp of all 16 jobs
-        pprint(output)
+        #print("output object = ",output)
+        pprint(result)
