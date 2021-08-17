086a4ae21d37903f717f1e6161e7fc8cde6a4ddb
DamonLee5 <liwr5damon@gmail.com>
2021-08-16 20:36:30 -0400

    Add demo using sshcluster.

--- demo/demo_ssh_single.py
+++ demo/demo_ssh_single.py
@@ -5,9 +5,7 @@ import os
 from pprint import pprint
 # import dask.config
 # import dask.distributed
-from dask.distributed import Client, as_completed
-
-from dask_jobqueue import SLURMCluster
+from dask.distributed import Client, as_completed,SSHCluster
 
 # dask.config.set({"distributed.admin.tick.limit":'3h'})
 def slow_increment(x):
@@ -18,19 +16,19 @@ def slow_increment(x):
             'time': time.strftime("%H:%M:%S")}
 
 if __name__ == '__main__':
-    cluster = SLURMCluster(project='bouman',processes=5 ,n_workers=20,walltime='01:00:00', memory='7 GB',
-                           death_timeout=60, job_extra=['--nodes=20', '--ntasks-per-node=1'],
-                           cores=20)
-
-    cluster.scale(jobs=2)
-    print(cluster.job_script())
+    cluster = SSHCluster(
+    ["localhost", "localhost", "localhost", "localhost","brown-a028","brown-a028","brown-a028","brown-a205","brown-a205","brown-a205","brown-a257","brown-a257","brown-a257"],
+    connect_options={"known_hosts": None},
+    worker_options={"nthreads": 8},
+    scheduler_options={"port": 0, "dashboard_address": ":8797"}
+)
     client = Client(cluster)
 
     nb_workers = 0
     while True:
         nb_workers = len(client.scheduler_info()["workers"])
         print('Got {} workers'.format(nb_workers))
-        if nb_workers >= 10:
+        if nb_workers >= 12:
             break
         time.sleep(1)
 
@@ -38,6 +36,6 @@ if __name__ == '__main__':
 
     print('client:', client)
 
-    for future in as_completed(client.map(slow_increment, range(0,200))): # FIX
+    for future in as_completed(client.map(slow_increment, range(0,800))): # FIX
         result = future.result()
         pprint(result)
